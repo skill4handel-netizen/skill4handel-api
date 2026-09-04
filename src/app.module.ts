e0307edcb0c4a7a4527ee3c2d0b64222b3c550e2
Aryaa <skill4handel@gmail.com>
2026-09-04 13:58:07 +0200

    Fix users module path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { MatchModule } from './match/match.module';
 import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, ChatModule, MatchModule, AdminModule],
+  imports: [AuthModule, ChatModule, MatchModule, AdminModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
